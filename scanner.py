9fff29042856bd6d0491e9f0a743e4de8e3c672f
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-05-18 13:19:34 +0200

    rework scan feature

--- scanner.py
+++ scanner.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 from collections import defaultdict
+import hashlib
+import itertools
 
 
 class BtnFunc:
@@ -9,10 +11,13 @@ class BtnFunc:
         self.files = []
         self.file_size_dict = defaultdict(list)
         self.duplicate_dict = {}
+        self.hash_match = defaultdict(list)
+        self.duplicate_match = defaultdict(list)
+        self.first_chunk = defaultdict(list)
 
 
     def scan_device(self):
-        all_elements = self.system_root.rglob('*')
+        all_elements = itertools.chain(self.system_root.rglob('*.pdf'), self.system_root.rglob('*.docx'))
         for i in all_elements:
             try:
                 if i.is_file():
@@ -29,9 +34,34 @@ class BtnFunc:
                 pass
 
 
-    def get_duplicates(self):
+    def get_duplicate_sizes(self):
         self.duplicate_dict = {k:v for (k, v) in self.file_size_dict.items() if len(v) > 1}
 
+
+    def find_duplicates(self):
+        for i in self.duplicate_dict.values():
+            for j in i:
+                hasher = hashlib.sha256()
+                try:
+                    with open(j, 'rb') as f:
+                        chunk = f.read(8192)
+                        hasher.update(chunk)
+                        self.hash_match[hasher.hexdigest()].append(j)
+                except (PermissionError, FileNotFoundError, OSError):
+                    pass
+        self.first_chunk = {k:v for (k, v) in self.hash_match.items() if len(v) > 1}
+        for i in self.first_chunk.values():
+            for j in i:
+                hasher = hashlib.sha256()
+                try:
+                    with open(j, 'rb') as f:
+                        chunk = f.read(8192)
+                        while chunk:
+                            hasher.update(chunk)
+                            chunk = f.read(8192)
+                    self.duplicate_match[hasher.hexdigest()].append(j)
+                except (PermissionError, FileNotFoundError, OSError):
+                    pass
 
 
 
@@ -39,6 +69,7 @@ class BtnFunc:
 if __name__ == '__main__':
     btns = BtnFunc()
     btns.scan_device()
-    btns.get_size_dict()
-    btns.get_duplicates()
-    print(len(btns.duplicate_dict))
+    # btns.get_size_dict()
+    # btns.get_duplicate_sizes()
+    # btns.find_duplicates()
+    print(len(btns.files))
